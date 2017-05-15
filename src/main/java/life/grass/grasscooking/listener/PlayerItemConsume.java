13d470ee1a41b01861a8b106c1fc89282363f8e9
unicroak <unicroak@gmail.com>
2017-05-15 16:30:23 +0900

    [fix] スタミナ回復時の表示バグを修正

--- src/main/java/life/grass/grasscooking/listener/PlayerItemConsume.java
+++ src/main/java/life/grass/grasscooking/listener/PlayerItemConsume.java
@@ -1,26 +1,47 @@
 package life.grass.grasscooking.listener;
 
 import life.grass.grassplayer.GrassPlayer;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class PlayerItemConsume implements Listener {
 
     @EventHandler
     public void onPlayerItemConsume(PlayerItemConsumeEvent event) {
-        ItemStack item = event.getItem();
+        Player player = event.getPlayer();
+        PlayerInventory inv = player.getInventory();
+        ItemStack item = event.getItem().clone();
 
-        if (!true /* check it is not Food*/) {
-            event.setCancelled(true);
+        event.setCancelled(true);
+
+        if (!true /* check it is not Food */) {
+            return;
+        }
+
+        if (item.getAmount() < 2) {
+            item.setType(Material.AIR);
+        } else {
+            item.setAmount(item.getAmount() - 1);
+        }
+
+        if (inv.getItemInMainHand().equals(event.getItem())) {
+            inv.setItemInMainHand(item);
+        } else if (inv.getItemInOffHand().equals(event.getItem())) {
+            inv.setItemInOffHand(item);
+        } else {
             return;
         }
 
         GrassPlayer grassPlayer = GrassPlayer.findOrCreate(event.getPlayer());
 
         // TODO: change
-        grassPlayer.incrementStamina(5);
         grassPlayer.incrementEffectiveStamina(5);
+
+        player.updateInventory();
     }
 }

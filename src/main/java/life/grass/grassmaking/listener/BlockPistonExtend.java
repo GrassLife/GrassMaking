e082b01ffad1c629ede756aca96d07c239c2b808
unicroak <unicroak@gmail.com>
2017-07-02 02:10:28 +0900

    [fix] ピストンでテーブルを押し出せないように

--- src/main/java/life/grass/grassmaking/listener/BlockPistonExtend.java
+++ src/main/java/life/grass/grassmaking/listener/BlockPistonExtend.java
@@ -2,7 +2,6 @@ package life.grass.grassmaking.listener;
 
 import life.grass.grassmaking.GrassMaking;
 import life.grass.grassmaking.manager.TableManager;
-import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,12 +18,11 @@ public class BlockPistonExtend implements Listener {
 
     @EventHandler
     public void onBlockPistonExtend(BlockPistonExtendEvent event) {
-        if (event.getBlocks().size() < 1) return;
-        Block pushed = event.getBlocks().get(0);
-
-        Arrays.asList(
-                pushed,
-                pushed.getRelative(BlockFace.UP)
-        ).forEach(block -> tableManager.findTable(block).ifPresent(table -> event.setCancelled(true)));
+        event.getBlocks().forEach(pushed ->
+                Arrays.asList(
+                        pushed,
+                        pushed.getRelative(BlockFace.UP)
+                ).forEach(block -> tableManager.findTable(block).ifPresent(table -> event.setCancelled(true)))
+        );
     }
 }
